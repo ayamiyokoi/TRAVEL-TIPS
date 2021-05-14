6eac3b43e02fd5666a76dfe76adc67c33a674ed6
AyamiYokoi <didufd63hff@gmail.com>
2021-05-14 03:13:59 +0000

    [Add]toppage

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'tips/index'
-  get 'tips/show'
-  get 'tips/new'
-  get 'tips/edit'
-  get 'homes/top'
+resources :tips
+root :to => "homes#top"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
